1d48ab969a19e01388d4fb5bf6a45f4878e5e448
eyapene <Administrateur@WORKSTATION>
2017-12-16 03:14:14 +0000

    Correctifs sur l'interface solveur après rapport checkstyle

--- src/main/java/solveur/ISolveur.java
+++ src/main/java/solveur/ISolveur.java
@@ -8,13 +8,15 @@ package solveur;
  * @author BOKOBOSSO Eyapènè
  *
  */
-public interface ISolveur {
+public interface ISolveur {  
+    
+    
     /**
      * Teste la validité de la grille.
      *
      * @return boolean
      */
-    boolean verifierGrille();
+    boolean verifierGrille();  
 
     /**
      * Résoud la grille passée en paramètre.
@@ -23,7 +25,7 @@ public interface ISolveur {
      * valable ou si aucune solution n'a pu être calculée
      * @return boolean
      */
-    boolean resoudre();
+    boolean resoudre();  
 
     /**
      * Verifie si la grille est résolue.
@@ -31,10 +33,10 @@ public interface ISolveur {
      *@throws IllegalArgumentException si la grille non valide
      *@return boolean
      */
-    boolean resolu();
+    boolean resolu();  
 
     /**
      * Affiche la solution trouvée à la grille.
      */
-    void afficherSolution();
+    void afficherSolution();  
 }
